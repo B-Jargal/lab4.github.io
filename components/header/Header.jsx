b09513b2243d10e97f3882129ce390a924bc19fe
Jargal <sjargal09@gmail.com>
2021-04-21 00:00:27 +0800

    lab4

---
+++ components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Header.css"
+
+const Header = () => {
+
+    return (
+        <div>
+            <div className="head">
+                <div>
+                    <img src='components/header/1.png' alt="" className='pro'/>
+                </div>
+            </div>
+        </div>
+    )
+} 
+export default Header;
